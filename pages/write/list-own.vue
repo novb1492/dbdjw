7dd51418c8efb4e05d24cc20f2e406187a16e5e8
novb1492 <novb1492@naver.com>
2023-04-22 11:48:34 +0900

    20230422

--- pages/write/list-own.vue
+++ pages/write/list-own.vue
@@ -9,21 +9,37 @@ import { listOwnRequest } from '../../api/articleApi';
 import { instance, setInterceptors } from '../../api/request';
 import { error403, ffsfs, getReToken, getReTokenAsyncDate } from '../../assets/jsLib';
 export default {
-    async asyncData({ isDev, route, store, env, params, query, req, res, redirect, error, $axios }) {
-        let response = null;
-        console.log("fdfdf");
-        try {
-            response = await $axios.$get(`${process.env.API_URL}/api/article/list?page=${1}&pageSize=${1}&title=${1}&text=${1}`, { withCredentials: true });
-        } catch (error) {
-            console.log(error);
-            // return redirect('/login');
+    data() {
+        return {
+            retryCount: 0
         }
-
-
     },
-    middleware:'renewToken',
+    async mounted() {
+        this.requestWithRetry({page:this.$route.query.page,pageSize:this.$route.query.pageSize,title:this.$route.query.title,text:this.$route.query.text});
+    },
     methods: {
-
+        async requestWithRetry(data) {
+            try {
+                const response = await listOwnRequest(data);
+                console.log(response);
+                // this.insertDoneF(response);
+            } catch (error) {
+                const { status } = error.response;
+                if (status === 401) {
+                    if (this.retryCount < 1) {
+                        this.retryCount = 1;
+                        if (await getReToken()) {// 엑세스 토큰 재발급
+                            return this.requestWithRetry(data); // 재발급 후 다시 요청
+                        }
+                    }
+                    error403();//재발급 횟수 초과
+                } else if (status === 403) {//재발급실패
+                    error403();
+                } else {
+                    error403();
+                }
+            }
+        },
     }
 }
 </script>
